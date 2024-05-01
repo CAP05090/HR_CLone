b41521f9921b5e756921a6bc7a39ccd266b1f81c
CAP05090 <ksand7398@gmail.com>
2024-05-01 20:11:58 +0530

    Embrace ai page

--- frontend/src/pages/homepage/Solutions/EmbraceAI.jsx
+++ frontend/src/pages/homepage/Solutions/EmbraceAI.jsx
@@ -6,7 +6,82 @@ export const EmbraceAI = () => {
   return (
     <>
     <Navbar />
-    <div>EmbraceAI</div>
+    <div>
+      <div>
+        <h1>Embrace the future of tech with HackerRank AI</h1>
+        <p>The AI Revolution is here, and developer skills will never be the same. As AI reshapes what it means to write code, we’re re-inventing how to evaluate and improve coding skills—thanks to AI-assisted assessments, robust fairness and integrity features, and a new hiring process grounded in AI.</p>
+        <p>Get early access to new HackerRank AI features</p>
+      </div>
+      <img src="https://www.hackerrank.com/wp-content/uploads/2023/05/Hero.png" alt="hero" />
+    </div>
+    <div>
+      <button>Introducing HAckerRank AI</button>
+    </div>
+    <div>
+      <div>
+        <img src="https://www.hackerrank.com/wp-content/uploads/2023/02/terminal.svg" alt="terminal" />
+        <h2>AI-Integrated Assessments</h2>
+        <p>Bring AI programming assistants into the assessment experience, enabling developers to showcase their skills with the latest technologies.</p>
+      </div>
+      <div>
+        <img src="https://www.hackerrank.com/wp-content/uploads/2023/02/eye.svg" alt="eye" />
+        <h2>Assessment Integrity</h2>
+        <p>Find tech skills with confidence with AI-powered plagiarism detection that uses dozens of discrete signals to ensure fairness. Plus, we flag and remove easily-solved questions from our library.</p>
+      </div>
+      <div>
+        <img src="https://www.hackerrank.com/wp-content/uploads/2023/02/repeat.svg" alt="repeat" />
+        <h2>Hiring Productivity</h2>
+        <p>Boost your hiring effectiveness. AI-derived signals like code quality and candidate effort analysis give teams the insights they need to make informed decisions every step of the way.</p>
+      </div>
+    </div>
+    <div>
+      <h2>AI-Integrated Assessments</h2>
+      <h1>Embrace AI in real-world challenges</h1>
+      <p>AI is changing how developers work. Our next-generation assessments bring AI into the workflow:</p>
+      <ul>
+        <li>AI assistants like copilot and ChatGPT built directly into our IDE</li>
+        <li>New question types built with AI in mind</li>
+        <li>Customizable AI access levels to match your AI readiness</li>
+      </ul>
+      <img src="https://www.hackerrank.com/wp-content/uploads/2023/05/2023-05-05-17.24.54.gif" alt="gif" />
+    </div>
+    <div>
+      <h2>Assessment Integrity</h2>
+      <h1>Ensure fair and accurate assessments</h1>
+      <p>Leverage AI to protect the fairness and accuracy of assessments:</p>
+      <ul>
+        <li>Best-in-class AI-powered plagiarism detection uses dozens of discrete signals to monitor assessments</li>
+        <li>Four times more accurate than traditional plagiarism detection models</li>
+        <li>Automatically remove questions easily solved by AI from our question library and all future tests</li>
+      </ul>
+      <img src="https://www.hackerrank.com/wp-content/uploads/2023/05/Ensure-fair-and-accurate-assesments.png" alt="faire and accurate assement" />
+    </div>
+    <div>
+      <h2>Hiring Productivity</h2>
+      <h1>Let AI improve your hiring process</h1>
+      <p>We’re embedding AI assistance throughout our platform to make you more productive.</p>
+      <ul>
+        <li>AI-derived signals like code quality and effort analysis provide key insights every step of the way.</li>
+        <li>AI assistance reduces time-consuming tasks like question writing</li>
+        <li>Automated Interview transcription and more</li>
+      </ul>
+      <img src="https://www.hackerrank.com/wp-content/uploads/2023/05/2023-05-05-17.42.28.gif" alt="gif" />
+    </div>
+    <div>
+      <div>
+        <h1>And that’s just the tip of the AI-ceberg</h1>
+        <p>AI opens up all kinds of opportunities in technical hiring and skills management. For all we’ve done, we’re just getting started. We’re cooking up some exciting new things in our lab and can’t wait to share more with you soon.</p>
+      </div>
+      <img src="https://www.hackerrank.com/wp-content/uploads/2023/05/Iceberg.png" alt="iceberg" />
+    </div>
+    <div>
+      <img src="https://www.hackerrank.com/wp-content/uploads/2023/05/Dev-skills.png" alt="dev skills" />
+      <div>
+      <h2>Developer Skills: AI Report</h2>
+      <h1>82% of developers think AI will redefine the future of coding</h1>
+      <p>Our Developer Skills: AI report explores how developers are adopting AI—and how companies are shifting their hiring practices in response.</p>
+      </div>
+    </div>
     <Footer />
     </>
   )
