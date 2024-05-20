33436714e57cef031050436514d8e36c9ca2c862
CAP05090 <ksand7398@gmail.com>
2024-05-20 11:24:09 +0530

    logout handlers

--- frontend/src/Context/authContext.jsx
+++ frontend/src/Context/authContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { useEffect, createContext, useState } from "react";
+import axios from 'axios';
 
 export const AuthContext = createContext();
 
@@ -6,8 +7,31 @@ export const AuthContextProvider = ({children})=>{
     const [login, setLogin] = useState(false);
     const [token, setToken] = useState("")
 
+    useEffect(()=>{
+        const StoredToken = localStorage.getItem("token")
+        if (StoredToken){
+            setLogin(true)
+            setToken(StoredToken)
+        }
+    }, []);
+
+    const logoutHandler = async () => {
+        try {
+          const response = await axios.post('http://localhost:8080/users/logout',  {token} );
+          if (response.data) {
+            localStorage.removeItem('token');
+            setLogin(false);
+            setToken("")
+          }
+          alert(response.data)
+          console.log(response.data)
+        } catch (error) {
+          console.error('Error:', error.message);
+        }
+      };
+
     return(
-        <AuthContext.Provider value={{login, setLogin, token, setToken}}>
+        <AuthContext.Provider value={{login, setLogin, token, setToken, logoutHandler}}>
             {children}
         </AuthContext.Provider>
     )
